b7189ffebb43f7474013a1fda52de3b88cc09458
netsanet77 <netsanetbikes@gmail.com>
2022-06-30 16:35:45 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-/*c programming 
- * print using puts
- */
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
