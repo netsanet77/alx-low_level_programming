8e7b56d0c6b3e7d4309a378766406a50877ed291
netsanet77 <netsanetbikes@gmail.com>
2022-06-30 17:24:56 -0700

    my commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu ", (unsigned long)sizeof(a), "byte(s)\n");
+printf("Size of an int: %lu ", (unsigned long)sizeof(b), "byte(s)\n");
+printf("Size of a long int: %lu ", (unsigned long)sizeof(c), "byte(s)\n");
+printf("Size of a long long int: %lu ", (unsigned long)sizeof(d), "byte(s)\n");
+printf("Size of a float: %lu ", (unsigned long)sizeof(e), "byte(s)\n");
+return (0);
+}
