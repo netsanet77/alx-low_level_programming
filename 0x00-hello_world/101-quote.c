dac2d5c493820961584d637963ca2723cc8075f9
netsanet77 <netsanetbikes@gmail.com>
2022-06-30 18:26:29 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+while (*a)
+putchar (*a++);
+return (1);
+}
