fe5d51589f4dec133d1e7e11a11f46465f8fb59a
netsanet77 <netsanetbikes@gmail.com>
2022-07-04 05:26:12 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastdigit = n % 10;
 printf("Last digit of %d is %d %s\n", n, lastdigit, lastdigit > 5 ?
-"and is greater than 5" lastdigit < 6 && lastdigit != 0 ?
+"and is greater than 5" : lastdigit < 6 && lastdigit != 0 ?
 "and is less than 6 and not 0" : "and is 0");
 return (0);
 }
