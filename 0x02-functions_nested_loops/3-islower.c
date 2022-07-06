3053d1ef2ea4a6d4273f43ff9d28389ce74f9d78
netsanet77 <netsanetbikes@gmail.com>
2022-07-06 08:03:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - checks whether the letter is lowercase or not
+ *
+ * Return: int
+ */
+int _islower(int c)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (c == i)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+}
