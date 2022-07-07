a81677b986609d80deb388e1c8c327f0447382ec
netsanet77 <netsanetbikes@gmail.com>
2022-07-06 18:02:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isalpha - checks whether the the character is letter or not
+ *@c: the character to be check
+ *
+ * Return: int
+ */
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122 && c >= 60 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
