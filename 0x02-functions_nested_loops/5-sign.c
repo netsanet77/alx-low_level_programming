9be75fe7c2956915a95b11b12c36a0fcfda08732
netsanet77 <netsanetbikes@gmail.com>
2022-07-06 18:39:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+/**
+ * print_sign - print the sign of the number
+ *@n: the given number
+ *
+ * Return: int
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar(0);
+return (0);
+}
+}
