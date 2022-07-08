f4c408438424569b33d6c9863cc33a2d70a6001e
netsanet77 <netsanetbikes@gmail.com>
2022-07-07 17:49:56 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - check whether the character is digit or not
+ *@c: character to be check
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+if (c <= 9 && c >= 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
