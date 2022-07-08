2e4f2a446c6c7e8c02ca4bb751a3ee30ae1dfefe
netsanet77 <netsanetbikes@gmail.com>
2022-07-07 19:57:50 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - draw a diagonal line
+ *@n: the number of times the character should be printed
+ * 
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+_putcahr('\n');
+}
