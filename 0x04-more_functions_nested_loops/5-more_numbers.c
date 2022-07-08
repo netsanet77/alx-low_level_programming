5bf30de5de11fd600bc2f9c514de5ab2b1326d6b
netsanet77 <netsanetbikes@gmail.com>
2022-07-08 02:46:31 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,13 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-_putchar(j + '0');
+if (j < 10)
+{
+_putchar(j % 10 + '0');
+}
+else
+{
+_putchar(j / 10 * 10 + '0');
 }
 _putchar('\n');
 }
