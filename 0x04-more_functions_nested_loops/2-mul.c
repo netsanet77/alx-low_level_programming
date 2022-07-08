d15e43a3b08eb0186427012541869de0415ec27c
netsanet77 <netsanetbikes@gmail.com>
2022-07-07 18:06:25 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - check whether the character is digit or not
+ *@a: character to be check
+ *@b: character to be check
+ *
+ * Return: 1 or 0
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 void _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
