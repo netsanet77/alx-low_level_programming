4648cf42dc06720851ce0154936fb1809a97824f
netsanet77 <netsanetbikes@gmail.com>
2022-07-07 19:07:03 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 0 to 14, 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+}
