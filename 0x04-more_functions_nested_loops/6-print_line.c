297efcd120fccd3b881b5b39f2190edfad91c475
netsanet77 <netsanetbikes@gmail.com>
2022-07-07 19:22:10 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putcahr('\n');
+}
