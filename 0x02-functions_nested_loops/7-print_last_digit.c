b598b1c18f36d0c6b9593018b40f41ac31686573
netsanet77 <netsanetbikes@gmail.com>
2022-07-08 03:23:40 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * print_last_digit - print the last digit of the number
+ *@c: the given number
  *
  * Return: int
  */
 int print_last_digit(int c)
 {
+_putchar(c % 10 + '0');
 return (c % 10);
 }
