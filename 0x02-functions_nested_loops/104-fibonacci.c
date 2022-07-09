d2f053253ce4ba31131002817e3ba948ea4185a6
netsanet77 <netsanetbikes@gmail.com>
2022-07-09 08:00:27 -0700

    commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int n1, n2, nxt;
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, ", n1);
+	for (i = 1; i < 98; i++)
+	{
+		if (i != 97)
+		{
+			printf("%ld, ", n2);
+		}
+		else
+		{
+			printf("%ld\n", n2);
+		}
+		nxt = n1 + n2;
+		n1 = n2;
+		n2 = nxt;
+	}
+	return (0);
+}
