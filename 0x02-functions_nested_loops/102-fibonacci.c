830255d5a3a8d781ffd33fce02ef6e7f5e73db60
netsanet77 <netsanetbikes@gmail.com>
2022-07-09 07:24:50 -0700

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+int i, n1, n2, nxt;
+i = 1;
+n1 = 1;
+n2 = 2;
+printf("%d, ", n1);
+while (i < 50)
+{
+printf("%d, ", n2);
+nxt = n1 + n2;
+n1 = n2;
+n2 = nxt;
+i++;
+}
+return (0);
+}
