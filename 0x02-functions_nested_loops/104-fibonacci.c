1fea3e3c2c5156afd9735b6d7f084d4dad457748
netsanet77 <netsanetbikes@gmail.com>
2022-07-09 08:12:00 -0700

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,19 +6,19 @@
 int main(void)
 {
 int i;
-double n1, n2, nxt;
+unsigned long int n1, n2, nxt;
 n1 = 1;
 n2 = 2;
-printf("%ld, ", n1);
+printf("%lu, ", n1);
 for (i = 1; i < 98; i++)
 {
 if (i != 97)
 {
-printf("%ld, ", n2);
+printf("%lu, ", n2);
 }
 else
 {
-printf("%ld\n", n2);
+printf("%lu\n", n2);
 }
 nxt = n1 + n2;
 n1 = n2;
