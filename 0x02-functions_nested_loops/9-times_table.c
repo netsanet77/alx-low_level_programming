d12802f2beeef8e88aa39dcb36ffd9bfb3b186f5
netsanet77 <netsanetbikes@gmail.com>
2022-07-09 04:30:35 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,31 @@
  */
 void times_table(void)
 {
-int i, j;
+int i, j, result;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar(i * j + '0');
-if (j != 9)
+result = i * j;
+if (j == 0)
+{
+_putchar(result + '0');
+}
+else
+{
+if (result < 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+_putchar(result + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(result + '0');
+}
 }
 }
 _putchar('\n');
