55a24ef5ba7661c1e5317355a43246a9d20d69a0
netsanet77 <netsanetbikes@gmail.com>
2022-07-10 08:31:30 -0700

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,6 +8,12 @@
 void print_square(int size)
 {
 int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -15,5 +21,6 @@ for (j = 0; j < size; j++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
