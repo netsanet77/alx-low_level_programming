f5a8ae69bac4002b73af869c8a5ab705cdfe2939
netsanet77 <netsanetbikes@gmail.com>
2022-07-11 05:20:25 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers
+ * @a: pointer
+ * @b: pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
