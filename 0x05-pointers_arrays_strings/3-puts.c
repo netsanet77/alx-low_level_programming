0125c44d1e46957fa7fa1c3f33f7856901715fcb
netsanet77 <netsanetbikes@gmail.com>
2022-07-12 16:25:11 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the given string
+ * Return: void
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; s[i] != 0; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
