d0247022721c2423bb1367ddce26e39b8d7ac76a
netsanet77 <netsanetbikes@gmail.com>
2022-07-12 16:40:25 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the given string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i;
+for (i = strlen(s); s[i] != 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
