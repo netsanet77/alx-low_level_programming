f744f4861a55313f9458dd07df2d3dea8ad79996
netsanet77 <netsanetbikes@gmail.com>
2022-07-13 02:48:04 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copies the string pointed to by src to buffer pointed to by dest
+ * @dest: the pointer that points the destination of the string
+ * @src: the pointer that points the source of the string
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
