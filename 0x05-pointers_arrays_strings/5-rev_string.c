0cc5f6deb1eec167357d98c517d971d4860ecdbd
netsanet77 <netsanetbikes@gmail.com>
2022-07-13 01:09:58 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse the string
+ * @s: the given string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int i, j, a;
+char *b;
+a = strlen(s);
+j = 0;
+for (i = a - 1; s[i] != 0; i--)
+{
+b[j] = s[i];
+}
+strcpy(s, b);
+}
