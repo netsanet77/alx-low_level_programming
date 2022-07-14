bca8962ac451d3cae7635fc8b9d58859c29b43de
netsanet77 <netsanetbikes@gmail.com>
2022-07-14 00:26:14 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: the destination  pointer
+ * @src: the source  pointer
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
