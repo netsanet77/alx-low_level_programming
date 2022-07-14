d8cc083617acea7e120972f6959e7ea9b49c6171
netsanet77 <netsanetbikes@gmail.com>
2022-07-14 02:08:10 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compare two strings
+ * @s1: given string
+ * @s2: given string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int n;
+n = strcmp(s1, s2);
+return (n);
+}
