1837aa6098a4c05fcf4dcfa00da7c3af837a9849
netsanet77 <netsanetbikes@gmail.com>
2022-07-15 00:14:24 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: the given string
+ * Return: character
+ */
+char *cap_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i - 1] == ',' || s[i - 1] == ';'
+|| s[i - 1] == '.' || s[i - 1] == '?'
+|| s[i - 1] == '"' || s[i - 1] == '('
+|| s[i - 1] == ')' || s[i - 1] == '{'
+|| s[i - 1] == '}' && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
+}
