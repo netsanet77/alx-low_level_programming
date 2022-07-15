9184390e923ba0da91ae8f7632d5e6ebae9f0bc6
netsanet77 <netsanetbikes@gmail.com>
2022-07-15 09:55:20 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: the given string
+ * Return: char
+ */
+char *rot13(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] <= 'Z' && s[i] >= 'A'))
+{
+if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+{
+s[i] = s[i] + 13;
+}
+else
+{
+s[i] = s[i] - 13;
+}
+}
+}
+return (s);
+}
