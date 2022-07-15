fe2019f4fea2691b777ded55b50fff9428bcaf6b
netsanet77 <netsanetbikes@gmail.com>
2022-07-15 05:52:38 -0700

    commiti

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+for (i = 0; src[i] != '\0' && i < n; i++)
 {
 dest[i] = src[i];
-if (i == 89)
+}
+while (i < n)
 {
-dest[i + 1] = '\0';
+dest[i] = '\0';
+i++;
 }
-}
+
 return (dest);
 }
