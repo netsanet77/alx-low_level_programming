8dfe39909a3a6fa6b8b481822d10f9ad4927014f
netsanet77 <netsanetbikes@gmail.com>
2022-07-14 19:11:30 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: the given array
+ * @n: the number of elements of the array
+ * Retern: nothing
+ */
+void reverse_array(int *a, int n)
+{
+int i, j, temp;
+j = strlen(a);
+for (i = 0; a[i] != '\0'; i++)
+{
+temp = a[j - 1];
+a[j - 1] = a[i];
+a[i] = temp;
+}
+}
