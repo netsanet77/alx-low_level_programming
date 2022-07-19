094dccd0e390e6103b9508c0e5593504a0dcb2fd
netsanet77 <netsanetbikes@gmail.com>
2022-07-18 21:17:33 -0700

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,21 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-int i, j;
-char *nstr;
-j = 0;
+int i;
 for (i = 0;  s[i] != '\0'; i++)
 {
 if (s[i] == c)
 {
-while (s[i] != '\0')
-{
-nstr[j] = s[i];
-j++;
-i++;
-}
-break;
+return (s);
 }
 }
-return (nstr);
 }
