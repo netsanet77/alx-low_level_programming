86addf3e95445fe448e57cde85ed7bbe3da1b351
netsanet77 <netsanetbikes@gmail.com>
2022-07-18 19:52:36 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the pointer that points the memory area
+ * @b: the value that fills the memory
+ * @n: the first n bytes of memory
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*s[i] = b;
+}
+return (s);
+}
