52eb97367837e2fee36241ededbfb8b5fa8f9dda
netsanet77 <netsanetbikes@gmail.com>
2022-07-20 06:29:39 -0700

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-int i = 0;
 if (*s == '\0')
 {
-return (i);
+return (0);
 }
-i++;
-s++;
-_strlen_recursion(s);
-return (i);
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
 }
