0e29e450d7879bf32a022c9252acdabb3075c8ee
netsanet77 <netsanetbikes@gmail.com>
2022-07-25 08:30:45 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ */
+int main(int argc, char* argv[])
+{
+if (argv[argc - 1] != '\0')
+ printf("%d", (argc - 1));
+	return 0;
+}
