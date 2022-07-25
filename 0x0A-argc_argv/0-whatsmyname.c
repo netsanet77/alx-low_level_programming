20cf2db206c1473278348d75acf3b8d93f3d2c38
netsanet77 <netsanetbikes@gmail.com>
2022-07-25 07:53:32 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  entry point
+ * @argc: the size of the array
+ * @args: given array
+ * Return: Always 0
+ *
+ */
+int main(int argc, char *args[])
+{
+printf("%s\n", args[argc - 1]);
+return (0);
+}
