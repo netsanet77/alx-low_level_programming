682955b6c05bf8c3ac2268268cfc3b7757d0fc5f
netsanet77 <netsanetbikes@gmail.com>
2022-07-26 00:23:11 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - entry point\
+ * main - entry point
  * @argc: the size of the array
  * @argv: given array
  * Return: 1 and 0
@@ -14,7 +14,7 @@ if (argc == 3)
 {
 for (i = 1; i < argc; i++)
 {
-result = result * atoi(argv[i]);
+result *= atoi(argv[i]);
 }
 printf("%d\n", result);
 return (0);
