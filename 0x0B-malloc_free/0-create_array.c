414889742dc0a91d77b7b672b2ae4cee307b828a
netsanet77 <netsanetbikes@gmail.com>
2022-07-26 03:52:52 -0700

    first use of malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *p;
+p = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return (p);
+}
