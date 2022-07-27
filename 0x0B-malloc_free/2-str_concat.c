3e32523a55c945307bb588f99eeb6e2e17978352
netsanet77 <netsanetbikes@gmail.com>
2022-07-27 02:07:18 -0700

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * str_concat - return the concatination of two strings given
+ * as the parameter
+ * @s1: the given string comes first after concatination
+ * @s2: the last given string
+ * Return: pointer of character
+ */
+char *str_concat(char *s1, char *s2)
+{
+int x, y;
+char *p;
+x = strlen(s1);
+y = strlen(s2);
+p = malloc((x + y + 1) * sizeof(char));
+if (!p)
+	return (NULL);
+strcpy(p, s1);
+strcat(p, s2);
+return (p);
+}
