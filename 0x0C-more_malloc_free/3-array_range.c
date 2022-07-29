ef108c27a4fca2425d98bd5fe1a08bb01eda5b63
netsanet77 <netsanetbikes@gmail.com>
2022-07-29 01:52:24 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *arr, i, size;
 if (min > max)
 	return (NULL);
 size = max - min + 1;
-arr = malloc(size * sizeof(int));
+arr = malloc(size *sizeof(int));
 if (!arr)
 	return (NULL);
 for (i = 0; i < size; i++)
