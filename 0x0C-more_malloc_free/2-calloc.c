9086f7998136797f22ef22460110b96b35e5c4ea
netsanet77 <netsanetbikes@gmail.com>
2022-07-29 01:03:57 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,12 @@ if (nmemb == 0 || size == 0)
 {
 	return (NULL);
 }
-p = malloc((nmemb + 1) * sizeof(size));
+p = malloc(nmemb * sizeof(size));
 if (!p)
 {
 	return (NULL);
 }
-for (i = 0; i < nmemb; i++)
+for (i = 0; i < (nmemb * size); i++)
 {
 	p[i] = 0;
 }
