e763aa038d9d363068ab50a64343fd78f131ed02
netsanet77 <netsanetbikes@gmail.com>
2022-08-03 01:48:38 -0700

    pointer to a function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - function that prints a name
+ * @name: the given name
+ * @f: pointer to point the function
+ * Return: nothing
+ *
+ */
+void print_name(char *name, void(*f)(char *))
+{
+if (name != NULL)
+	f(name);
+}
