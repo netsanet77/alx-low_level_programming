c5e202273b3b458076e81ce229e427562d57a01c
netsanet77 <netsanetbikes@gmail.com>
2022-08-04 13:37:53 -0700

    commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_char - print the character
+ * @ap: arguments of print_all function
+ * Return: nothing
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_integer - print the integer
+ * @ap: arguments of print_all function
+ * Return: nothing
+ */
+void print_integer(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - print the floating number
+ * @ap: arguments of print_all function
+ * Return: nothing
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - print the string
+ * @ap: arguments of print_all function
+ * Return: nothing
+ */
+void print_string(va_list ap)
+{
+	char *c;
+
+	c = va_arg(ap, char *);
+
+	if (c != NULL)
+		printf("%s", c);
+	else
+		printf("(nil)");
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+int k = 0, j;
+va_list ap;
+
+print_t print[] = {
+	{"c", print_char},
+	{"i", print_integer},
+	{"f", print_float},
+	{"s", print_string},
+	{NULL, NULL}
+};
+
+va_start(ap, format);
+while (format[k])
+{
+	j = 0;
+	while (j < 4)
+	{
+		if (*print[j].type == format[k])
+		{
+			print[j].p(ap);
+			if (format[k + 1])
+				printf(", ");
+			break;
+		}
+		j++;
+	}
+	k++;
+}
+printf("\n");
+va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct printType - structure for type of parameter
+ * @type: type of the argument
+ * @p: pointer to function
+ *
+ */
+typedef struct printType
+{
+	char *type;
+	void (*p)(va_list);
+} print_t;
+
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+void print_string(va_list);
 #endif
