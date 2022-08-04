21b3b02e73806be02c081d8996b320ac3a096666
netsanet77 <netsanetbikes@gmail.com>
2022-08-03 19:57:20 -0700

    commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,9 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ * op_add - sum of a and b
+ * @a: one of the number
+ * @b: another number
+ *
+ * Return: sum of the number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ * @a: one of the number
+ * @b: another number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of a and b
+ * @a: one of the number
+ * @b: another number
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - result of the division of a by b
+ * @a: one of the number
+ * @b: another number
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder division of a by b
+ * @a: one of the number
+ * @b: another number
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
