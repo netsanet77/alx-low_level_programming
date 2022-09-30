a67fd21bcb7cc0fd00742671f86e605b5d711261
netsanet <netsanetbikes@gmail.com>
2022-09-30 02:53:16 -0700

    correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,6 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new_node->next = temp->next;
 			new_node->prev = temp;
+			temp->next->prev = new_node;
 			temp->next = new_node;
 			return (new_node);
 		}
