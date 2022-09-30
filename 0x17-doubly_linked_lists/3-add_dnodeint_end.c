26196670a38207451a4343164832f1edf5749c9e
netsanet <netsanetbikes@gmail.com>
2022-09-30 01:00:38 -0700

    correction

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,8 +9,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *temp = *head;
+	dlistint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -20,18 +19,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (*head == NULL)
 	{
-		new_node->prev = NULL;
 		*head = new_node;
 		return (*head);
 	}
+	temp = *head;
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
 	temp->next = new_node;
 	new_node->prev = temp;
-	free(new_node);
-	return (temp);
+	return (new_node);
 }
