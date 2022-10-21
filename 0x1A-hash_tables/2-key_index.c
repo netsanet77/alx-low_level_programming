77fb8b31a22721eacf88d487681fc1da17a0ba83
netsanet <netsanetbikes@gmail.com>
2022-10-21 13:06:00 -0700

    commit

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function that gives you the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should
+ * be stored in the array of the hash table
+ *
+ * Description: use djb2 algorithm
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
